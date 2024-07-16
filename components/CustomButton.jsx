02b85dc541bcff1fcaa0829698a01fe6ce323ed7
Nazmus Sakib Mouin <nsmouin7@gmail.com>
2024-07-16 18:51:24 +0600

    change text color in CustomButton component

--- components/CustomButton.jsx
+++ components/CustomButton.jsx
@@ -17,7 +17,7 @@ const CustomButton = ({
 			}`}
 			disabled={isLoading}
 		>
-			<Text className={`text-primary font-psemibold text-lg ${textStyles}`}>
+			<Text className={`text-white font-psemibold text-lg ${textStyles}`}>
 				{title}
 			</Text>
 		</TouchableOpacity>
