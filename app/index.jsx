8cd0bd90ab6cd6212d95267787e96f2888091468
Nazmus Sakib Mouin <nsmouin7@gmail.com>
2024-07-16 18:50:46 +0600

    add CustomButton component

--- app/index.jsx
+++ app/index.jsx
@@ -3,7 +3,9 @@
 
 import { StatusBar } from "expo-status-bar";
 import { ScrollView, Text, View, Image } from "react-native";
+import { Redirect, router } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
+import CustomButton from "../components/CustomButton";
 
 export default function App() {
 	return (
@@ -11,6 +13,13 @@ export default function App() {
 			<ScrollView contentContainerStyle={{ height: "100%" }}>
 				<View className="w-full justify-center items-center min-h-[85vh] px-4">
 					<Text className="text-white text-3xl">This is e-Icon Group 7</Text>
+					<CustomButton
+						title="Continue with Email"
+						handlePress={() => {
+							router.push("/home");
+						}}
+						containerStyles="w-full mt-7"
+					/>
 				</View>
 			</ScrollView>
 			<StatusBar />
