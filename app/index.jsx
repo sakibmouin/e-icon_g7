d8d9b5e2706e097ab4e784a304324d6b0c816815
Nazmus Sakib Mouin <nsmouin7@gmail.com>
2024-07-16 18:43:40 +0600

    create landing screen

--- app/index.jsx
+++ app/index.jsx
@@ -7,8 +7,12 @@ import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
 	return (
-		<SafeAreaView>
-			<ScrollView></ScrollView>
+		<SafeAreaView className="bg-primary h-full flex-1 items-center justify-center">
+			<ScrollView contentContainerStyle={{ height: "100%" }}>
+				<View className="w-full justify-center items-center min-h-[85vh] px-4">
+					<Text className="text-white text-3xl">This is e-Icon Group 7</Text>
+				</View>
+			</ScrollView>
 			<StatusBar />
 		</SafeAreaView>
 	);
