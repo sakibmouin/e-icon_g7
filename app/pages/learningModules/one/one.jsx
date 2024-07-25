2aac6e9b2255bf4dd100397730cb53b91dd24cb1
Nazmus Sakib Mouin <nsmouin7@gmail.com>
2024-07-26 04:35:07 +0600

    add module page template

--- app/pages/learningModules/one/one.jsx
+++ app/pages/learningModules/one/one.jsx
@@ -1,23 +1,20 @@
 import {
 	View,
 	Text,
-	StyleSheet,
+	ScrollView,
 	Image,
 	TouchableOpacity,
 	FlatList,
-	ScrollView,
 } from "react-native";
 import React, { useState } from "react";
-import { router } from "expo-router";
+import { useRouter } from "expo-router";
 import CustomButton from "../../../../components/CustomButton";
-
-const tabs = ["About", "Lessons", "Mentor"];
 
 const data = {
 	name: "Innovative Startups: Sustainable Growth Strategies with SDG",
 	aboutCourse:
 		"Discover how to launch and grow your startup with a focus on sustainability and inclusive economic growth. This course will guide you through essential strategies to build a successful business aligned with Sustainable Development Goal 8. Learn how to conduct market research, secure funding, develop eco-friendly products, and create a positive impact on your community. Perfect for aspiring entrepreneurs committed to making a difference while achieving business success. Join us to drive innovation and sustainability in your startup journey!",
-	benifits: [
+	benefits: [
 		{ id: 1, title: "Sustainable Growth Techniques" },
 		{ id: 2, title: "Market Research Skills" },
 		{ id: 3, title: "Funding Insights" },
@@ -28,51 +25,14 @@ const data = {
 
 const one = () => {
 	const [showFullText, setShowFullText] = useState(false);
+	const router = useRouter();
 
 	const toggleFullText = () => {
 		setShowFullText(!showFullText);
 	};
 
-	// const [activeTab, setActiveTab] = useState(tabs[0]);
-
-	// const displayTabContent = () => {
-	// 	switch (activeTab) {
-	// 		case "About":
-	// 			return (
-
-	// 			);
-
-	// 		case "Lessons":
-	// 			return (
-	// 				<Specifics
-	// 					title="Qualifications"
-	// 					points={data[0].job_highlights?.Qualifications ?? ["N/A"]}
-	// 				/>
-	// 			);
-
-	// 		case "Responsibilities":
-	// 			return (
-	// 				<Specifics
-	// 					title="Responsibilities"
-	// 					points={data[0].job_highlights?.Responsibilities ?? ["N/A"]}
-	// 				/>
-	// 			);
-
-	// 		default:
-	// 			return null;
-	// 	}
-	// };
-
 	return (
 		<View className="bg-primary h-full px-4">
-			<View className="absolute bottom-4 w-full z-10 self-center">
-				<CustomButton
-					title="Open Course"
-					handlePress={() => {
-						router.push("pages/learningModules/one/lessons");
-					}}
-				/>
-			</View>
 			<ScrollView>
 				<View className="px-4 mb-4">
 					<Image
@@ -109,8 +69,8 @@ const one = () => {
 							Benefits
 						</Text>
 						<FlatList
-							data={data.benifits}
-							keyExtractor={(item) => item.$id}
+							data={data.benefits}
+							keyExtractor={(item) => item.id.toString()}
 							renderItem={({ item }) => (
 								<View className="flex flex-row items-center gap-2">
 									<Text className="text-2xl text-white -translate-y-0.5">{`\u25CF`}</Text>
@@ -123,6 +83,14 @@ const one = () => {
 					</View>
 				</View>
 			</ScrollView>
+			<View className="absolute bottom-4 w-full z-10 self-center">
+				<CustomButton
+					title="Open Course"
+					handlePress={() => {
+						router.push("pages/learningModules/one/lessons");
+					}}
+				/>
+			</View>
 		</View>
 	);
 };
